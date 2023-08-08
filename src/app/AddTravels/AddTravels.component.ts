8faf680cdd1ecb4cd93f4578875cbcca55222365
Parthasarathy-E <113752672+Parthasarathy-E@users.noreply.github.com>
2023-08-09 01:19:47 +0530

    Revert "Revert "Develop""

--- src/app/AddTravels/AddTravels.component.ts
+++ src/app/AddTravels/AddTravels.component.ts
@@ -3,51 +3,61 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-AddTravels',
   templateUrl: './AddTravels.component.html',
   styleUrls: ['./AddTravels.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatProgressSpinnerModule]
+  imports: [CommonModule, FormsModule, MatProgressSpinnerModule],
 })
 export class AddTravelsComponent implements OnInit {
   busData: any = {
-    Bus_Number: "",
-    Date: "",
-    Destination: "",
-    Seats:[],
-    Source: "",
-    Travels_Name: "",
+    Bus_Number: '',
+    Date: '',
+    Destination: '',
+    Seats: [],
+    Source: '',
+    Travels_Name: '',
     amount: 100,
     status: true,
-  }
+  };
   isEdit: Boolean = false;
   loading: Boolean = false;
-  constructor(private route: ActivatedRoute, private router: Router , private http: HttpClient) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    private admin: AdminService
+  ) {}
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
     this.isEdit = id != 'add';
-    if(this.isEdit){
-      this.http.get('http://localhost:3000/BusList/' + id).subscribe(res => this.busData = Object.assign({}, res));
+    if (this.isEdit) {
+      this.admin
+        .getBusById(id)
+        .subscribe((res) => (this.busData = Object.assign({}, res)));
     }
   }
-  addOrUpdateTravels(){
-    let alertText = this.busData.Travels_Name + ' (' + this.busData.Bus_Number + ')';
-    if(this.isEdit){
-      this.http.patch<any>('http://localhost:3000/BusList/' + this.busData.id, this.busData).subscribe(res => {
-        alert(alertText + " Updated !!!");
-      })
-    }
-    else{
-      this.http.post<any>('http://localhost:3000/BusList/', this.busData).subscribe(res => {
-        alert(alertText + " Added !!!");
-      })
+  addOrUpdateTravels() {
+    let busName =
+      this.busData.Travels_Name + ' (' + this.busData.Bus_Number + ')';
+    if (this.isEdit) {
+      this.admin
+        .updateBusById(this.busData.id, this.busData)
+        .subscribe((res) => {
+          alert(busName + ' Updated !!!');
+        });
+    } else {
+      this.admin.addBusToList(this.busData).subscribe((res) => {
+        alert(busName + ' Added !!!');
+      });
     }
     setTimeout(() => {
       this.router.navigate(['/admindashboard']);
-    },2000);
+    }, 2000);
   }
 }
--- src/app/SeatSelectedUsersDetails/SeatSelectedUsersDetails.component.ts
+++ src/app/SeatSelectedUsersDetails/SeatSelectedUsersDetails.component.ts
@@ -1,55 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-SeatSelectedUsersDetails',
   templateUrl: './SeatSelectedUsersDetails.component.html',
   styleUrls: ['./SeatSelectedUsersDetails.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class SeatSelectedUsersDetailsComponent implements OnInit {
+  public userDetailsForm!: FormGroup;
   SelectedSeats!: any;
   seatSelected!: Array<any>;
   filteredSeat: any;
 
-  isLogedIn: Boolean = false //
-  constructor( private route: ActivatedRoute,private router: Router, private http: HttpClient) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    private user: UserService
+  ) {}
 
   ngOnInit() {
+    this.userDetailsForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('[A-Za-z]{3,}')]],
+      age: ['', Validators.required, Validators.pattern('[1-9]{2}')]    
+    });
+
     // let qureyStr = this.route.snapshot.paramMap.get('value');
     // this.SelectedSeats = JSON.parse(String(qureyStr));
-    this.http.get<any>("http://localhost:3000/selectedBus/1")
-    .subscribe(res=>{
-     this.SelectedSeats =  res;
-     this.seatSelected = this.SelectedSeats?.Seats.map((a: any) => Object.assign(a, {name: a?.name ? a.name : '' , age: a?.age ? a.age : '', gender: a?.gender ? a.gender : "Male"}));
-     this.filteredSeat = this.seatSelected.filter((a:any) => !a.booked)
+    this.user.getSelectedBusDetails().subscribe((res) => {
+      this.SelectedSeats = res;
+      this.seatSelected = this.SelectedSeats?.Seats.map((a: any) =>
+        Object.assign(a, {
+          name: a?.name ? a.name : '',
+          age: a?.age ? a.age : '',
+          gender: a?.gender ? a.gender : 'Male',
+        })
+      );
+      this.filteredSeat = this.seatSelected.filter((a: any) => !a.booked);
     });
-    this.http.get('http://localhost:3000/userDetails/1').subscribe(res => { //
-      this.isLogedIn = res.hasOwnProperty('uid');   //
-    });  //
   }
-  goTo(){
+  userDetails() {
+    
+  }
+  goTo() {
     // router
-    this.http.patch<any>("http://localhost:3000/selectedBus/1", Object.assign(this.SelectedSeats, {Seats: this.seatSelected})).subscribe(res=>{
-console.log(res)
-  });
-this.router.navigate(["/bookingconfirmation"])
-  console.log(Object.assign(this.SelectedSeats, {Seats: this.seatSelected}));
-  }
-  go_To(toPath: any){  //
-    this.router.navigate([toPath]) //
-  } //
-  logout(){ //
-    this.http.put('http://localhost:3000/userDetails/1', {id: 1}).subscribe(res => { //
-      alert('Logout Successfully');
-      setTimeout(() => {
-        this.go_To('/');
-      },2000);
-    });
+    this.user
+      .updateSelectedBusDetails(
+        Object.assign(this.SelectedSeats, { Seats: this.seatSelected })
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+    this.router.navigate(['/bookingconfirmation']);
+    console.log(
+      Object.assign(this.SelectedSeats, { Seats: this.seatSelected })
+    );
   }
 }
